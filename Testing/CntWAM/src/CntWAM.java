46fa699a4c098bb1cee64bfc0f00094365b9bc9c
inga-zimnya <78633859+inga-zimnya@users.noreply.github.com>
2023-01-29 22:10:49 +0300

    Add files via upload

---
+++ Testing/CntWAM/src/CntWAM.java
@@ -0,0 +1,10 @@
+public class CntWAM {
+    public static void main(String[] args) {
+        int days = 234;
+        int weeks = days / 7;
+        int months = weeks / 4;
+
+        System.out.println("Total number of weeks: " + weeks);
+        System.out.println("Total number of months: " + months);
+    }
+}
